9bf5bf6dbeb55b1e335ff1201b646d5ea8753e7d
Amjad Salah <amjad.alzubeir@gmail.com>
2019-09-10 05:43:39 +0200

    UI review

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('meetings/', views.meetings, name='meetings'),
+    path('articles/', views.articles, name='articles'),
+    path('announcements/', views.announcements, name='announcements'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+  return render(request, 'pages/index.html')
 
 
 def meetings(request):
-    return render(request, 'pages/meetings.html')
+  return render(request, 'pages/meetings.html')
+
+
+def articles(request):
+  return render(request, 'pages/articles.html')
+
+
+def announcements(request):
+  return render(request, 'pages/announcements.html')
