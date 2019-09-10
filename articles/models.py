5de69d7d2b941968c54365ab94872ed546a9e4bd
Amjad Salah <amjad.alzubeir@gmail.com>
2019-09-10 14:31:44 +0200

    Ckeditor

--- articles/models.py
+++ articles/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Article(models.Model):
   writer = models.ForeignKey(User, on_delete=models.DO_NOTHING)
   title = models.CharField(max_length=200)
-  body = RichTextField()
+  body = RichTextUploadingField()
   created_date = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('meetings/', include('meetings.urls')),
     path('articles/', include('articles.urls')),
     path('announcements/', include('announcements.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
