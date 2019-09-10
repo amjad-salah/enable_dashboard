84ae4b6be494b57c7792d3145b0977e5cd0474ac
Amjad Salah <amjad.alzubeir@gmail.com>
2019-09-10 13:31:47 +0200

    Edit

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Article(models.Model):
+  writer = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+  title = models.CharField(max_length=200)
+  body = RichTextField()
+  created_date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.title
--- meetings/models.py
+++ meetings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Meeting(models.Model):
+  owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+  subject = models.CharField(max_length=200)
+  agenda = RichTextField()
+  date = models.DateTimeField()
+
+  def __str__(self):
+    return self.subject
+
