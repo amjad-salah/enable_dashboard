38232c3e54762b54af31643a5ffeaa3dc88e9daa
Amjad Salah <amjad.alzubeir@gmail.com>
2019-09-11 08:49:37 +0200

    Show one article

--- articles/views.py
+++ articles/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from .models import Article
 
 def index(request):
-  articles = Article.objects.all().order_by('-created_date')
+  articles = Article.objects.filter(publish=True).order_by('-created_date')
   context = {
     'articles': articles
   }
   return render(request, 'articles/articles.html', context)
 
 def article(request, id):
-  return render(request, 'articles/article.html')
+  article = Article.objects.get(id=id)
+  context = {
+    'article': article
+  }
+  return render(request, 'articles/article.html', context)
