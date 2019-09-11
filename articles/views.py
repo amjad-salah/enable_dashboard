449f16f109b9b11c6e0b5f7bf792ea615eda6a15
Amjad Salah <amjad.alzubeir@gmail.com>
2019-09-11 10:34:01 +0200

    Pagination in articles page

--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Article
 
 def index(request):
   articles = Article.objects.filter(publish=True).order_by('-created_date')
+  paginator = Paginator(articles, 15)
+  page = request.GET.get('page')
+  paged_articles = paginator.get_page(page)
   context = {
-    'articles': articles
+    'articles': paged_articles
   }
   return render(request, 'articles/articles.html', context)
 
