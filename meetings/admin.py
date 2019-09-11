ed209a49a11d59192fe36f7bc31a82a1efd3479e
Amjad Salah <amjad.alzubeir@gmail.com>
2019-09-11 11:39:28 +0200

    Home page layout

--- meetings/admin.py
+++ meetings/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Meeting
 
 class MeetingAdmin(admin.ModelAdmin):
-  list_display = ('id', 'subject', 'owner', 'date')
+  list_display = ('id', 'subject', 'owner', 'date','finished')
   list_display_links = ('id', 'subject')
+  list_editable = ('finished',)
   list_per_page = 25
 
 admin.site.register(Meeting, MeetingAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from announcements.models import Announcement
+from meetings.models import Meeting
+from articles.models import Article
+
 
 def index(request):
-  return render(request, 'pages/index.html')
+  announcements = Announcement.objects.order_by('-created_date')[:3]
+  meetings = Meeting.objects.order_by('-date').filter(finished=False)[:3]
+  articles = Article.objects.order_by('-created_date').filter(publish=True)[:3]
+
+  context = {
+    'announcements': announcements,
+    'meetings': meetings,
+    'articles': articles
+  }
+  return render(request, 'pages/index.html', context)
 
 
 
