71f076c655ba8721d003f97e8394b2072b72722e
Amjad Salah <amjad.alzubeir@gmail.com>
2019-09-18 09:24:48 +0200

    Complete missions template function

--- missions/views.py
+++ missions/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Mission
 
 def index(request):
-  return render(request, 'missions/missions.html')
+  missions = Mission.objects.all()
+  context = {
+    'missions': missions
+  }
+  return render(request, 'missions/missions.html', context)
 
 def mission(request, id):
-  return render(request, 'missions/mission.html')
+  mission = Mission.objects.get(id=id)
+  context = {
+    'mission': mission
+  }
+  return render(request, 'missions/mission.html', context)
